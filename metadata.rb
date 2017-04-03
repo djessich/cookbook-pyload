d4eb84b8ecca54aa2bb92bf5fa47703c58f4fadb
Robb Kidd <robb@thekidds.org>
2017-04-03 10:52:16 -0400

    strip the read-in version number of extra whitespace
    Just in case the VERSION file gets a newline added back in by an
    enthusiastic text editor.
    
    ```
    > IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
    => "1.2.0\n"
    > IO.read(File.join(File.dirname(__FILE__), 'VERSION')).chomp
    => "1.2.0"
    ```
    
    Signed-off-by: Robb Kidd <robb@thekidds.org>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'projects@gridtec.at'
 license 'Apache 2.0'
 description 'Installs/Configures Pyload'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
+version IO.read(File.join(File.dirname(__FILE__), 'VERSION')).chomp
 
 %w(debian ubuntu centos redhat fedora suse opensuse arch freebsd).each do |os|
   supports os
