ea1bc89c44390d18aa09fc05a9189a265c1ff961
Dominik Jessich <dominik.jessich@chello.at>
2019-03-10 01:25:03 +0100

    fixes missing java dependency on rhel and suse

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -17,7 +17,7 @@
 # On FreeBSD remove package curl first, if it should be installed
 package 'curl' do
   action :remove
-  only_if { node['platform_family'].eql?('freebsd') && node['pyload']['packages'].include?('curl') }
+  only_if { platform_family?('freebsd') && node['pyload']['packages'].include?('curl') }
 end
 
 # Install all configured packages
@@ -25,9 +25,23 @@ node['pyload']['packages'].each do |pkg|
   package pkg
 end
 
+# When rhino has been installed (as specified in attrbiutes), ensure that
+# java is installed as dependency of rhino on RHEL and SUSE unless java has
+# already been installed
+package 'install java-headless dependency for rhino' do
+  case node['platform_family']
+  when 'rhel'
+    package_name 'java-headless'
+  when 'suse'
+    package_name 'java-1_8_0-openjdk-headless'
+  end
+  only_if { platform_family?('rhel', 'suse') && node['pyload']['packages'].include?('rhino') }
+  not_if 'command -v java >/dev/null 2>&1 || exit 1'
+end
+
 # On FreeBSD symlink python executeable in /usr/local/bin after python package
 # has been installed
 link '/usr/local/bin/python' do
   to '/usr/local/bin/python2.7'
-  only_if { node['platform_family'].eql?('freebsd') }
+  only_if { platform_family?('freebsd') }
 end
