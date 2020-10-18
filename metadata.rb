bc295d9ade719a87a39aa3aea71710d69b18ed58
djessich <dominik.jessich@chello.at>
2020-10-18 04:00:39 +0200

    drops offical support for suse and related platforms

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,7 @@ version '1.4.0'
 source_url 'https://github.com/djessich/cookbook-pyload'
 issues_url 'https://github.com/djessich/cookbook-pyload/issues'
 
-%w(ubuntu debian redhat centos fedora suse opensuse opensuseleap oracle).each do |os|
+%w(redhat centos fedora oracle debian ubuntu).each do |os|
   supports os
 end
 
